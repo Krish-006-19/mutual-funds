4c5301e7f2539a2de15f37ccaae99f84dc5fef09
Krish Ramena <krish.ramena24@vit.edu>
2026-06-22 18:00:10 +0530

    Update user.router.js

--- router/user.router.js
+++ router/user.router.js
@@ -4,13 +4,11 @@ const user = require("../models/user.models.js");
 const {
   registerUser,
   loginUser,
-  updateUser,
   deleteUser,
 } = require("../controllers/users.controllers.js");
 const { verifyToken } = require("../middleware/auth.middleware.js");
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-router.patch("/update", verifyToken, updateUser);
 router.delete("/delete", verifyToken, deleteUser);
 
 module.exports = router;
