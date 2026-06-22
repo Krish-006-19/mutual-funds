196d32ce31bedc08a0ba919222d63dc8128ac04c
Krish Ramena <krish.ramena24@vit.edu>
2026-06-22 17:59:52 +0530

    Update users.controllers.js

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -4,23 +4,6 @@ const trade = require("../models/trade.models");
 const bcrypt = require("bcrypt");
 const { createToken } = require("../middleware/auth.middleware");
 
-// async function registerUser(req, res) {
-//   try {
-//     const { username, email, password } = req.body;
-//     if (!username || !email || !password) {
-//       return res.status(400).json({ error: "All fields are required" });
-//     }
-//     const hash = await bcrypt.hash(password, 7);
-//     const newUser = new user({ username, email, password: hash });
-//     await newUser.save();
-//     await Portfolio.create({ userId: newUser._id, remainingBalance: 10000, funds: [] });
-//     res.status(201).json({ message: "User created successfully" });
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).json({ error: "Internal server error" });
-//   }
-// }
-
 async function registerUser(req, res) {
   try {
     const { username, email, password } = req.body;
@@ -60,26 +43,6 @@ async function loginUser(req, res) {
   }
 }
 
-async function updateUser(req, res) {
-  try {
-    const id = req.user.userId;
-    const { username, email, password } = req.body;
-    const hash = await bcrypt.hash(password, 7);
-    const updatedUser = await user.findByIdAndUpdate(
-      id,
-      { username, email, password: hash },
-      { new: true },
-    );
-    if (!updatedUser) {
-      return res.status(404).json({ error: "User not found" });
-    }
-    res.json({ message: "User updated successfully", user: updatedUser });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "Internal server error" });
-  }
-}
-
 async function deleteUser(req, res) {
   try {
     const id = req.user.userId;
@@ -100,6 +63,5 @@ async function deleteUser(req, res) {
 module.exports = {
   registerUser,
   loginUser,
-  updateUser,
   deleteUser,
 };
