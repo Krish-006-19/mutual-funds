73b23df5082d571ec7c0f38eced8564c89263387
Krish-006-19 <krish.ramena24@vit.edu>
2026-07-08 14:36:59 +0530

    latest changes by adding caching to the confunctionality service

--- service/cronfunctionality.service.js
+++ service/cronfunctionality.service.js
@@ -1,24 +1,29 @@
 const axios = require("axios");
 const History = require("../models/api_History_Fetch.models");
+const redis = require("../service/redisSetup.service.js");
 async function updateAllFunds() {
   const funds = JSON.parse(process.env.FIFTY_FUNDS);
   let c = 1;
 
   for (const schemeCode of funds) {
     try {
-    const { data } = await axios.get(
-      `${process.env.FUND_HISTORY_API.replace("///", `/${schemeCode}`)}`,
-    );
-    if (!data || !data.data) {
-      console.error(`No data found for scheme code: ${schemeCode}`);
-      continue;
-    }
-    
-    await History.updateOne(
-      { schemeCode },
-      { schemeCode: schemeCode, data: data.data },
-      { upsert: true }
-    );
+      const { data } = await axios.get(
+        `${process.env.FUND_HISTORY_API.replace("///", `/${schemeCode}`)}`,
+      );
+      if (!data || !data.data) {
+        console.error(`No data found for scheme code: ${schemeCode}`);
+        continue;
+      }
+
+      await History.updateOne(
+        { schemeCode },
+        { schemeCode: schemeCode, data: data.data },
+        { upsert: true },
+      );
+
+      redis.set(`fund_history:${schemeCode}`, data.data, {
+        ex: 21600,
+      });
       console.log(`Updated ${c}`);
       c++;
     } catch (err) {
