ccdfef5ba39c1d30218378f371be2222fcf89c41
Krish Ramena <krish.ramena24@vit.edu>
2026-07-15 09:27:23 +0530

    Update history.cron.js

--- cron/history.cron.js
+++ cron/history.cron.js
@@ -2,10 +2,12 @@ const cron = require("node-cron");
 const axios = require("axios");
 const { updateAllFunds } = require("../service/cronfunctionality.service.js");
 const redis = require("../service/redisSetup.service.js");
-cron.schedule("0 0 * * *", async () => {
+cron.schedule("0 5 * * *", async () => {
   try { 
     await updateAllFunds();
   } catch (error) {
     console.error("Error in cron job:", error);
   }
+},{
+    timezone: "Asia/Kolkata",
 });
